db96660884129adf1825bc3ef6245948190e7177
Oskar Truffer <Oskar Truffer>
2017-06-20 13:44:54 +0200

    caching of people who are around.

---
+++ src/main.rs
@@ -0,0 +1,97 @@
+extern crate pcap;
+extern crate time;
+
+use pcap::Device;
+use std::thread;
+use std::sync::mpsc;
+use std::fmt::LowerHex;
+use std::fmt::Formatter;
+use std::collections::HashMap;
+use std::fmt;
+
+struct HexSlice<'a>(&'a [u8]);
+
+impl<'a> HexSlice<'a> {
+    fn new<T>(data: &'a T) -> HexSlice<'a>
+        where T: ?Sized + AsRef<[u8]> + 'a
+    {
+        HexSlice(data.as_ref())
+    }
+}
+
+// You can even choose to implement multiple traits, like Lower and UpperHex
+impl<'a> fmt::Display for HexSlice<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, byte) in self.0.iter().enumerate() {
+            // Decide if you want to pad out the value here
+            write!(f, "{:02x} ", byte)?;
+            if (i + 1) % 8 == 0 {
+                write!(f, "\n");
+            }else if (i + 1) % 4 == 0 {
+                write!(f, "  ");
+            }
+        }
+        Ok(())
+    }
+}
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    let mut mac_to_name = HashMap::new();
+    let mut last_call : HashMap<String, i64> = HashMap::new();
+
+    mac_to_name.insert([0x0, 0x28, 0xf8, 0x4c, 0x7b ,0x2e], String::from("Oskar's Thinkpad"));
+
+    thread::spawn(move || {
+        let mut cap = Device::lookup().unwrap().open().unwrap();
+
+        while let Ok(packet) = cap.next() {
+            let len = (&packet.data).len();
+            let mut data = Vec::new();
+            data.resize(len, 0);
+            data.clone_from_slice(&packet);
+            tx.send(data).unwrap();
+        }
+    });
+
+    while let Ok(packet) = rx.recv() {
+        let dest_hwadr = &packet[0..6];
+        let source_hwadr = &packet[6..12];
+
+        match mac_to_name.get(source_hwadr) {
+            Some(name) => {
+                call(&mut last_call, name);
+            }
+            None => {}
+        }
+
+        match mac_to_name.get(dest_hwadr) {
+            Some(name) => {
+                call(&mut last_call, name);
+            }
+            None => {}
+        }
+    }
+}
+
+fn call(last_call : &mut HashMap<String, i64>, caller: &String) {
+    let mut update = false;
+
+    match last_call.get(caller) {
+        Some(last_send) => {
+            if last_send + 10 < time::now().to_timespec().sec {
+                let greeting = format!{"Hello {}", caller};
+                println!{"{}", greeting};
+                update = true;
+            }
+        }
+        None => {
+            println!("{} is in the Area!", caller);
+            update = true;
+        }
+    }
+
+    if update {
+        last_call.insert(caller.clone(), time::now().to_timespec().sec);
+    }
+}
